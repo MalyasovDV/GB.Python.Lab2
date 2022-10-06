571f4cf01dd6d4385e4b3b9bedfb00ab86f6b5ea
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-10-06 14:09:06 +0800

    Task done

--- Task4.py
+++ Task4.py
@@ -4,6 +4,14 @@ def create_list(n):
         list.append(i)
     return list
 
+def multiplicate_positions(list):
+    result = 1
+    with open('positions.txt') as f:
+        for line in f:
+            result *= list[int(line)]
+    return result
+
 n = int(input())
 list = create_list(n)
 print(list)
+print(multiplicate_positions(list))
